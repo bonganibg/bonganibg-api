f00d42e08ab724edf4ebafb8a8e64b325ee47589
Bonganibg <bongani.gumbo@yahoo.com>
2022-10-06 16:11:32 +0200

    updated analytics to take in encoded message

--- services/routes/analyticsRoute.js
+++ services/routes/analyticsRoute.js
@@ -3,18 +3,41 @@ const router = express.Router();
 const Analytics = require('../models/analyticsModel');
 
 router.post('', (req,res) => {
-    const analytics = {
-        date: req.body.date,
-        note: req.body.note
+
+    if (isDataValid(req.body.data)){
+        const decoded = atob(req.body.data);
+        const analytics = JSON.parse(decoded);
+        new Analytics(analytics).save()
+        .then(results => {
+            res.status(204).json();
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(401).json();
+        });
     }
-    new Analytics(analytics).save()
-    .then(results => {
-        res.status(204).json();
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(401).json();
-    });
+    else 
+    {
+        res.status(400).json();
+    }
 });
+
+
+const isDataValid = (input) => {       
+    try{
+        
+        const decoded = atob(input);
+        const model = JSON.parse(decoded);
+        if (model.date !== undefined && model.note !== undefined)
+        {
+            return true;
+        }        
+    }    
+    catch(error){
+        console.log(error);
+    }
+    
+    return false;
+}
 
 module.exports = router;
